a238fe39925747a433435735a7009b6cc53ed050
sanjaijr19 <Sanjai@merkatintellekt.com>
2023-03-19 17:53:17 +0530

    add gender,user_active

--- skype_replica_project/skype_replica_app/models.py
+++ skype_replica_project/skype_replica_app/models.py
@@ -6,12 +6,14 @@ class Profile(mongoengine.Document):
     first_name = mongoengine.StringField(max_length=20)
     last_name = mongoengine.StringField(max_length=20)
     dob = mongoengine.DateTimeField()
+    gender = mongoengine.StringField(choices=['male','female','others'],max_length=10)
     email = mongoengine.EmailField(required=True,unique=True)
     phone_no = mongoengine.IntField()
     city = mongoengine.StringField()
     state = mongoengine.StringField()
     country = mongoengine.StringField()
     profile_picture = mongoengine.ImageField(collection_name='profile_pics', size_limit=20971520, thumbnail_size=(100,100,True))
+    user_active = mongoengine.BooleanField(default=True)
 
 
 
--- skype_replica_project/skype_replica_app/serializers.py
+++ skype_replica_project/skype_replica_app/serializers.py
@@ -4,7 +4,7 @@ from rest_framework_mongoengine.serializers import DocumentSerializer
 class Profileserializer(DocumentSerializer):
     class Meta:
         model = Profile
-        fields = ('id','username','first_name', 'last_name','dob', 'email', 'phone_no','city', 'state','country','profile_picture')
+        fields = ('id','username','first_name', 'last_name','dob','gender', 'email', 'phone_no','city', 'state','country','profile_picture','user_active')
 
     def create(self, validated_data):
         return Profile.objects.create(**validated_data)
